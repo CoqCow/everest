419dd105c8f753aa4c3702a0fd326c35240c4c64
niuruobing <niuruobing@xiaomi.com>
2019-08-02 13:57:53 +0800

    登录逻辑

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//app.js
+let utils = require('./utils/util')
 App({
   onLaunch: function () {
     // 展示本地存储能力
@@ -9,28 +9,11 @@ App({
     // 登录
     wx.login({
        success: res => {
+         console.log(res.code);
         // 这里可以拿到登录code
-        console.log(res.code);
-      
-       //请求我们后端的获取用户信息接口
-         wx.request({
-           url: 'http://127.0.0.1:1818/superman/user/green/getUserInfo', 
-           data: {
-             code: res.code
-           },
-           header: {
-             'content-type': 'application/json'
-           },
-           method: "POST",
-           success(res) {
-             //接口返回值
-             console.log("code:" + res.data.code)
-             console.log("msg:"+ res.data.msg)
-             console.log("openId:" + res.data.data)
-           }
-         })
+         //this.getUserInfo(res.code);
       }
-    })
+    }),
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -38,6 +21,7 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log("here");
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
@@ -50,6 +34,41 @@ App({
         }
       }
     })
+  },
+  getUserInfo: function (code) {
+    let that = this
+    let paramdata = {
+      code: code,
+      token: that.token
+    }
+    console.log(paramdata);
+    return utils.requestApi(`${this.globalReqUrl}/user/green/getUserInfo`, paramdata).then(
+      res => {
+        console.log("res:"+res.code);
+        code=res.code;
+        console.log(code);
+        if(200==code){
+          that.userInfo = res.data.user;
+          that.token = res.data.token;
+          console.log("success");
+        }
+
+       // wx.setStorageSync('globaluserInfo', res.data)
+       // console.log('缓存数据', wx.getStorageSync('globaluserInfo'))
+       // console.log(res, that, '用户接口请求')
+        return res.data
+      },
+      err => {
+        console.log('error', err)
+        if (1000 == err.code){
+          console.log("没有注册");
+        }else if (1001 == err.code) {
+          console.log("code过期");
+        }
+       // wx.removeStorageSync('globaluserInfo')
+        return err
+      }
+    )
   },
   globalData: {
     userInfo: null,
--- utils/util.js
+++ utils/util.js
@@ -16,20 +16,19 @@ const formatNumber = n => {
 
 
 function requestApi(reqfile, paramdata, method) {
-  let plugin_ptKey = plugin.getStorageSync('jdlogin_pt_key')
-  const ptKey = wx.getStorageSync('jdlogin_pt_key') || plugin_ptKey || '' // 登录标识
-  paramdata.clientType = 'xcx'
+  //let plugin_ptKey = plugin.getStorageSync('jdlogin_pt_key')
+ // const ptKey = wx.getStorageSync('jdlogin_pt_key') || plugin_ptKey || '' // 登录标识
+ //paramdata.clientType = 'xcx'
   return new Promise((resolve, reject) => {
     wx.request({
       url: reqfile,
       data: paramdata,
-      method: method,
+      method: "POST",
       header: {
-        Cookie: 'pt_key=' + ptKey,
-        "content-type": "application/x-www-form-urlencoded"
+        'content-type': 'application/json'
       },
       success: result => {
-        if (result.data.code == 0) {
+        if (result.data.code == 200) {
           resolve(result.data)
         } else {
           reject(result.data)
