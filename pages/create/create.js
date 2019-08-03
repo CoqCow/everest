3c2e7eb9656468a7340c2164943de44ecd22bb99
unknown <l2366051620@qq.com>
2019-08-03 11:07:02 +0800

    create获取用户昵称及头像

--- pages/create/create.js
+++ pages/create/create.js
@@ -1,6 +1,7 @@
 var app = getApp();
 Page({
   data: {
+    userInfo: {},
     item: {
       key: "",
       value: {
@@ -14,7 +15,43 @@ Page({
     isNew: false,
     focus: true
   },
-
+  //获取用户昵称及头像
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
   /**
    * 页面渲染事件
    */
@@ -58,7 +95,8 @@ Page({
         });
         //返回首页
         wx.navigateBack();
-      } else {
+      } 
+      else {
         wx.showToast({
           title: "保存失败"
         });
