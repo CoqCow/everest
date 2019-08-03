27ad96876fa9de345c15818c3fd2c20f2487a000
niuruobing <niuruobing@xiaomi.com>
2019-08-03 18:50:19 +0800

    登录/注册

--- app.js
+++ app.js
@@ -9,30 +9,26 @@ App({
     // 登录
     wx.login({
        success: res => {
-         console.log(res.code);
-        // 这里可以拿到登录code
-         //this.getUserInfo(res.code);
+         this.getUserInfo(res.code);
       }
     }),
-    // 获取用户信息
     wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              console.log("here");
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                this.globalData.userInfo = res.userInfo
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
               }
-            }
-          })
+            })
+          }
         }
-      }
     })
   },
   getUserInfo: function (code) {
@@ -44,15 +40,16 @@ App({
     console.log(paramdata);
     return utils.requestApi(`${this.globalReqUrl}/user/green/getUserInfo`, paramdata).then(
       res => {
-        console.log("res:"+res.code);
-        code=res.code;
-        console.log(code);
-        if(200==code){
-          that.userInfo = res.data.user;
-          that.token = res.data.token;
-          console.log("success");
-        }
+        //-----------这里是登录成功 要跳转到/pages/square/square页面-----------
+          console.log("res:"+res.code);
+         // that.userInfo = res.data.user;
+          //that.token = res.data.token;
+        // 获取用户信息
 
+        wx.redirectTo({
+            url: '/pages/square/square'
+          }),
+          console.log("success");
        // wx.setStorageSync('globaluserInfo', res.data)
        // console.log('缓存数据', wx.getStorageSync('globaluserInfo'))
        // console.log(res, that, '用户接口请求')
@@ -61,6 +58,7 @@ App({
       err => {
         console.log('error', err)
         if (1000 == err.code){
+          //-----------这里是没有注册 要跳转到/pages/index/index页面-----------
           console.log("没有注册");
         }else if (1001 == err.code) {
           console.log("code过期");
