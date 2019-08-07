2c07e60b48b81d8d6b4c0a11b21186684d74d7b5
unknown <l2366051620@qq.com>
2019-08-07 13:13:51 +0800

    样式

--- pages/user/user.js
+++ pages/user/user.js
@@ -11,63 +11,8 @@ Page({
   },
   data: {
     userInfo: null,
-    banner: null,
-    autoplay: true,
-    interval: 2000,
-    duration: 1000,
-    txtAds: null,
-    advertise: null
-  },
-  /*
-  * 首页banner
-  */
-  setBanner: function () {
-    let that = this;
-    util.fetch('http://api.cyb.kuaiqiangche.com/event/advertise/banner', function (data) {
-      that.setData({
-        banner: data.data
-      });
-    });
-  },
-  /**
-   * 首页文字广告
-   */
-  setTxtAds: function () {
-    let that = this;
-    util.fetch('http://api.cyb.kuaiqiangche.com/event/advertise/roll', function (data) {
-      that.setData({
-        txtAds: data.data[0]
-      });
-    });
-  },
-  /**
-   * 首页两块子banner
-   */
-  setSubBanner: function () {
-    let that = this;
-    util.fetch('http://api.cyb.kuaiqiangche.com/event/advertise/index', function (data) {
-      that.setData({
-        advertise: data.data
-      });
-    });
-  },
-  /**
-   * 模块入口
-   */
-  setModule: function () {
-
-  },
-  /**
-   * 入口
-   */
-  /*onLoad: function () {
-    var that = this;
-    that.setBanner();
-    that.setTxtAds();
-    that.setSubBanner();
-    that.setModule();
     
-  }*/
+  },
   onLoad: function () {
     //获取用户昵称及头像
     if (app.globalData.userInfo) {
