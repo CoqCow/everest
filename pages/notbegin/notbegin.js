55cee0bff7d5ce357454d447abb71a22bc456767
unknown <l2366051620@qq.com>
2019-08-12 10:15:51 +0800

    未开始

---
+++ pages/notbegin/notbegin.js
@@ -0,0 +1,53 @@
+// pages/notbegin/notbegin.js
+const app = getApp()
+var util = require("../../utils/util.js")
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    token: null,
+    total: null,
+    planList: null,
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    this.setData({
+      token: wx.getStorageSync("token")
+    });
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.getPlanListInfo();
+  },
+  getPlanListInfo: function () {
+    let paramdata = {
+      token: this.data.token,
+      type: 2,
+      status:[2]
+    }
+    return util.requestApi(`${app.globalReqUrl}/plan/apple/listPlan`, paramdata).then(
+      res => {
+        this.setData({
+          total: res.data.total,
+          planList: res.data.list
+        });
+        return res.data;
+      },
+      err => {
+        console.log('error', err)
+        return err
+      }
+    )
+  },
+  clickPlan: function (e) {
+    wx.navigateTo({
+      url: '/pages/detail/detail?pid=' + e.currentTarget.dataset.pid
+    })
+  }
+
+})
